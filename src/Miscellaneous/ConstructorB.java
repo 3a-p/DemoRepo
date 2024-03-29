693c7af1433b767a2c5399b0e5e656c3eb317977
papiya <bhattacharjeepapia4@gmail.com>
2024-03-29 23:59:27 +0530

    modified constructorB class

--- src/Miscellaneous/ConstructorB.java
+++ src/Miscellaneous/ConstructorB.java
@@ -13,10 +13,24 @@ public class ConstructorB extends ConstructorA{
 		//super(10,20);
 		System.out.println("B const");
 	}
+	
+	public ConstructorB(int i)
+	{
+		super(i);
+	}
+	
+	public ConstructorB(int i,int j)
+	{
+		super(i,j);
+	}
+	
 
+		
 	public static void main(String[] args) {
 // when Child class B object is created, B constructor is called but it checks if B extends A and then A constructor is called first and then B const is called.
 		ConstructorB obj = new ConstructorB();
+		ConstructorB obj1 = new ConstructorB(10);
+		ConstructorB obj2 = new ConstructorB(20,30);
 
 	}
 
